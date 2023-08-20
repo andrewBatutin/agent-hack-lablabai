80fd694fc94b3512aaf3fd3127c072b3ce24e50a
Andrii Batutin <andrii.batutin@shelf.io>
2023-08-20 18:38:07 +0200

    fix for client

--- src/taix/client.py
+++ src/taix/client.py
@@ -42,12 +42,6 @@ class MyWeaviate(Weaviate):
         return docs
 
 
-client = weaviate.Client(
-    url="http://localhost:8080",  # Replace with your endpoint
-    additional_headers={"X-OpenAI-Api-Key": os.environ["OPENAI_API_KEY"]},  # Replace with your inference API key
-)
-
-
 def get_batch_with_cursor(client, class_name, batch_size=20, class_properties=None, filter=None, cursor=None):
     query = client.query.get(class_name, class_properties).with_where(filter).with_limit(batch_size)
 
@@ -58,6 +52,12 @@ def get_batch_with_cursor(client, class_name, batch_size=20, class_properties=No
 
 
 def get_all_docs(features, filter):
+    client = weaviate.Client(
+        "http://localhost:8080",
+        auth_client_secret=weaviate.AuthApiKey(api_key=os.environ["WV_API_KEY"]),
+        additional_headers={"X-OpenAI-Api-Key": os.environ["OPENAI_API_KEY"]},
+    )
+
     docs = get_batch_with_cursor(
         client,
         DOC_CLASS,
